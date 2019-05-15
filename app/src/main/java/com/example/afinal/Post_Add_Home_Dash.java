c68f30e78037598d403ec18255bd368375deabfe
Niwantha Ranawaka <44175668+NiwanthaR@users.noreply.github.com>
2019-05-15 21:52:28 +0530

    Upload post image

--- app/src/main/java/com/example/afinal/Post_Add_Home_Dash.java
+++ app/src/main/java/com/example/afinal/Post_Add_Home_Dash.java
@@ -1,26 +1,39 @@
 package com.example.afinal;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
 
 public class Post_Add_Home_Dash extends AppCompatActivity {
 
+    private ImageView advertice_image;
     private Button get_location , go_back , submit_data;
     private EditText U_address , U_city , U_province , U_stair , U_rooms , U_bathroom , U_parking , U_keymoneyfee , U_mounthfee , U_renttime , U_discription ;
 
@@ -32,14 +45,46 @@ public class Post_Add_Home_Dash extends AppCompatActivity {
     private String h_address , h_city , h_province , h_stair , h_rooms , h_bathroom , h_kitchen , h_garage , h_parking , h_water , h_garbage , h_gateandwall , h_keymoney , h_keymoneyfee , h_mounthlyfee , h_renttime , h_discription , h_ownername ;
     public static  String Home_latitude , Home_longitude;
 
+    private FirebaseStorage firebaseStorage;
+    private StorageReference storageReference;
+    private static int PICK_IMAGE = 123;
+    Uri imagepath ;
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data.getData() != null)
+        {
+            imagepath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),imagepath);
+                advertice_image.setImageBitmap(bitmap);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post__add__home__dash);
 
         firebaseAuth = FirebaseAuth.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
 
-        //------------------------------------------------------------------------------------------------------------
+        //----------------------------------------------------part of load image---------------------------------------------------------
+
+        storageReference = firebaseStorage.getReference();
+        //StorageReference myref = storageReference.child("Home advertisement").child(h_ownername);
+
+
+        //-----------------------------------------part of load owner-------------------------------------------------------------------
         FirebaseDatabase firebaseDatabase= FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = firebaseDatabase.getReference().child("Profile").child(firebaseAuth.getUid());
 
@@ -57,9 +102,12 @@ public class Post_Add_Home_Dash extends AppCompatActivity {
         });
 
 
-        //-------------------------------------------------------------------------------------------------------------
-
+        //---------------------------------------------------load component----------------------------------------------------
         uiload();
+
+        //---------------------------------------------------------------------------------------------------------------------
+
+
 
         go_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,9 +141,22 @@ public class Post_Add_Home_Dash extends AppCompatActivity {
                 {
                     sendhousedata();
                     Toast.makeText(Post_Add_Home_Dash.this,"Your Post added Succesfull",Toast.LENGTH_SHORT).show();
+                    Clear_all();
                     startActivity(new Intent(Post_Add_Home_Dash.this,LoginUserDashboard.class));
                 }
 
+            }
+        });
+
+
+        advertice_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Image"),PICK_IMAGE);
             }
         });
 
@@ -105,6 +166,8 @@ public class Post_Add_Home_Dash extends AppCompatActivity {
 
     private void uiload()
     {
+        advertice_image = (ImageView)findViewById(R.id.myadd_img);
+
         get_location =(Button)findViewById(R.id.btn_locations);
         go_back =(Button)findViewById(R.id.btn_backtomenu);
         submit_data =(Button)findViewById(R.id.btn_submitshome);
@@ -240,8 +303,37 @@ public class Post_Add_Home_Dash extends AppCompatActivity {
 
         User_add_Homepost user_add_homepost = new User_add_Homepost(h_address,h_city,h_province,h_stair,h_rooms,h_bathroom,h_kitchen,h_garage,h_parking,h_water,h_garbage,h_gateandwall,h_keymoney,h_keymoneyfee,h_mounthlyfee,h_renttime,h_discription,Home_latitude,Home_longitude);
         homepost.setValue(user_add_homepost);
+
+        //----------------------------------------------------image part---------------------------------------------------
+        StorageReference imageRefarance = storageReference.child("Home advertise").child(h_ownername);
+        UploadTask uploadTask = imageRefarance.putFile(imagepath);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Post_Add_Home_Dash.this,"Image upload Failed",Toast.LENGTH_SHORT).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(Post_Add_Home_Dash.this,"Image upload successfully",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
+    private void Clear_all()
+    {
+        U_address.setText("");
+        U_city.setText("");
+        U_province.setText("");
+        U_stair.setText("");
+        U_rooms.setText("");
+        U_bathroom.setText("");
+        U_parking.setText("");
+        U_keymoneyfee.setText("");
+        U_mounthfee.setText("");
+        U_renttime.setText("");
+        U_discription.setText("");
 
+    }
 
 }

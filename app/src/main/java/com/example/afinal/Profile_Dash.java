cc1ba6ce8deb722d1a616aff0e69be3aa3e376c4
Niwantha Ranawaka <44175668+NiwanthaR@users.noreply.github.com>
2019-05-24 00:46:32 +0530

    Load Profile Picture

--- app/src/main/java/com/example/afinal/Profile_Dash.java
+++ app/src/main/java/com/example/afinal/Profile_Dash.java
@@ -1,6 +1,7 @@
 package com.example.afinal;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,12 +11,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 public class Profile_Dash extends AppCompatActivity {
 
@@ -25,6 +30,7 @@ public class Profile_Dash extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
     private FirebaseDatabase firebaseDatabase;
+    private FirebaseStorage firebaseStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +42,11 @@ public class Profile_Dash extends AppCompatActivity {
         //set fire base
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+
+
+
+
 
         //get uid
         DatabaseReference databaseReference = firebaseDatabase.getReference().child("Profile").child(firebaseAuth.getUid());
@@ -62,6 +73,21 @@ public class Profile_Dash extends AppCompatActivity {
            }
         });
 
+
+        //-------------------------------------image part-------------------------------------------
+        StorageReference storageReference = firebaseStorage.getReference();
+        storageReference.child("Profile Picture").child(firebaseAuth.getUid()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).fit().centerCrop().into(Profilepic);
+            }
+        });
+
+
+
+
+
+
         ProfileEdite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,5 +108,6 @@ public class Profile_Dash extends AppCompatActivity {
 
         ProfileEdite=(Button) findViewById(R.id.btnEditProfile);
 
+        Profilepic=(ImageView)findViewById(R.id.P_picture);
    }
 }
